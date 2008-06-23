826ef9bbaf6b2acb99b3eb19397ab9cc30340c7b
katherine.edwards2@mail.mcgill.ca <katherine.edwards2@mail.mcgill.ca@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2008-06-23 20:35:28 +0000

    Added functionality to LTILaunch.java -Added time checking to security -Logs in/creates user to create a session -Uses toolid data to create a launchURL -Added sakai dependencies to axis2/pom.xml & build path
    git-svn-id: https://source.sakaiproject.org/svn/webservices/branches/SAK-13392-B@48034 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- axis2/src/main/java/org/sakaiproject/axis2/LTILaunch.java
+++ axis2/src/main/java/org/sakaiproject/axis2/LTILaunch.java
@@ -3,28 +3,45 @@ package org.sakaiproject.axis2;
 import org.apache.axiom.om.OMElement;
 import org.apache.axiom.om.OMFactory;
 import org.apache.axiom.om.OMAbstractFactory;
+import org.apache.axis2.AxisFault;
+import org.apache.axis2.context.MessageContext;
 import java.io.UnsupportedEncodingException; 
 import java.security.NoSuchAlgorithmException; 
 import java.security.MessageDigest;
 
 import javax.xml.namespace.*;
+import org.sakaiproject.site.api.ToolConfiguration;
+import org.sakaiproject.site.cover.SiteService;
+import org.sakaiproject.site.api.Site;
+import org.sakaiproject.event.cover.UsageSessionService;
+import org.sakaiproject.exception.*;
+import org.sakaiproject.id.cover.IdManager;
+import org.sakaiproject.user.cover.UserDirectoryService;
+import org.sakaiproject.user.api.User;
+import org.sakaiproject.tool.api.Session;
+import org.sakaiproject.tool.cover.SessionManager;
+
+
 
 public class LTILaunch {
 	public OMFactory factory = OMAbstractFactory.getOMFactory();
+	private boolean CHECK_TIME = false;
+	private boolean CHECK_DIGEST = false;
+	private boolean CHECK_SITE = false;
+	private boolean CHECK_LOGIN = false;
+	private final String secret = "secret";
 	
 	public OMElement testlaunch(OMElement request) {
 		request.build();
 		request.detach();
 		
 		OMElement launchResponse = factory.createOMElement(new QName("launchResponse"));
+		OMElement status = factory.createOMElement(new QName("status"));
 		
-		OMElement siteID_el = request.getFirstChildWithName(new QName("toolID"));
-		String siteID = siteID_el.getText();
 		
 		OMElement toolID_el = request.getFirstChildWithName(new QName("toolID"));
 		String toolID = toolID_el.getText();
-		
-		String secret = "secret";
+		//launchResponse.addChild(toolID_el); 
 		
 		OMElement nonce_el = request.getFirstChildWithName(new QName("sec_nonce"));
 		String nonce = nonce_el.getText();
@@ -49,10 +66,34 @@ public class LTILaunch {
 		catch(NoSuchAlgorithmException e) {}
 		catch(UnsupportedEncodingException e) {}
 		
-		boolean success = digest.equals(sha1);
+		CHECK_DIGEST = digest.equals(sha1);
+		CHECK_TIME = TimeCalculator.within2Days(created);
 		
-		OMElement status = factory.createOMElement(new QName("status"));
+		String sessionId = null;
+		try {
+			sessionId = loginCreateSession(request.getFirstChildWithName(new QName("user_id")),
+					request.getFirstChildWithName(new QName("user_firstname")),
+					request.getFirstChildWithName(new QName("user_lastname")), 
+					request.getFirstChildWithName(new QName("user_email")));
+			CHECK_LOGIN = true;
+		}
+		catch(Exception e) {
+			CHECK_LOGIN = false;
+		}
 		
+		ToolConfiguration siteTool = SiteService.findTool(toolID);
+		CHECK_SITE = siteTool != null;
+		String siteId = null;
+		try {
+			siteId = siteTool.getSiteId();
+			CHECK_SITE = true;
+			
+		}
+		catch(Exception e) {
+			CHECK_SITE = false;
+		}
+	
+		boolean success = CHECK_DIGEST && CHECK_TIME && CHECK_SITE && CHECK_LOGIN;
 		
 		if (success) {
 			status.setText("success");
@@ -87,13 +128,32 @@ public class LTILaunch {
 				launchResponse.addChild(launchUrl);
 			}
 			
+			//this is just here for now, not sure where to put this data
+			OMElement test_launchURL = factory.createOMElement(new QName("TOOLURL"));
+			test_launchURL.setText("http://localhost:8080/portal/tool/" + toolID + "?sakai.session=" + sessionId);
+			launchResponse.addChild(test_launchURL);
 		}
 		else {
 			status.setText("fail");
 			OMElement code = factory.createOMElement(new QName("code"));
-			code.setText("BadPasswordDigest");
 			OMElement description = factory.createOMElement(new QName("description"));
-			description.setText("The password digest was invalid");
+			/* set error codes */
+			if (!CHECK_DIGEST) {
+				code.setText("BadPasswordDigest");
+				description.setText("The password digest was invalid");
+			}
+			else if (!CHECK_TIME) {
+				code.setText("BadRequestTime");
+				description.setText("The request created time was invalid");
+			}
+			else if (!CHECK_LOGIN) {
+				code.setText("LoginFail");
+				description.setText("Could not log user in");
+			}
+			else if (!CHECK_SITE) {
+				code.setText("BadSiteRequest");
+				description.setText("Could not find the requested site.");
+			}
 			
 			launchResponse.addChild(status);
 			launchResponse.addChild(code);
@@ -128,6 +188,69 @@ public class LTILaunch {
 		widget.setText("<object width=\"" + width +"\" height=\""+ height + "\"><param name=\"movie\" value=\"http://www.youtube.com/v/f90ysF9BenI&hl=en\"></param><embed src=\"http://www.youtube.com/v/f90ysF9BenI&hl=en\" type=\"application/x-shockwave-flash\" width=\"" + width + "\" height=\"" + height + "\"></embed></object>");
 		
 		return widget;
+	}
+	
+	
+	/**
+	 * 
+	 * @param eid_el
+	 * @param fname_el
+	 * @param lname_el
+	 * @param email_el
+	 * @return Session Id of created session.
+	 * @throws AxisFault
+	 */
+	private String loginCreateSession(OMElement eid_el, OMElement fname_el, OMElement lname_el, OMElement email_el) 
+	throws AxisFault {
+		eid_el.build();
+		eid_el.detach();
+		fname_el.build();
+		fname_el.detach();
+		lname_el.build();
+		lname_el.detach();
+		email_el.build();
+		email_el.detach();
+		
+		String eid = eid_el.getText();
+		String fname = fname_el.getText();
+		String lname = lname_el.getText();
+		String email = email_el.getText();
+		
+		MessageContext messageContext = MessageContext.getCurrentMessageContext();
+		String ipAddress = (String)messageContext.getProperty("REMOTE_ADDRESS");
+
+		User user = null;
+		try {
+			user = UserDirectoryService.getUserByEid(eid);
+		}
+		catch(Exception e) {
+			user = null;
+		}
+		
+		if (user == null && fname != null && lname != null && email != null) {
+			try {
+				String hiddenPW = IdManager.createUuid();
+				UserDirectoryService.addUser(null,eid,fname,lname,email,hiddenPW,"registered", null);
+				user = UserDirectoryService.getUserByEid(eid);
+			}
+			catch(Exception e) {
+				throw new AxisFault("Unable to create user.");
+			}
+			
+		}
+		
+		
+		Session s = SessionManager.startSession();
+		SessionManager.setCurrentSession(s);
+		
+		if (s == null) {
+			throw new AxisFault("Unable to create session.");
+		}
+		else {
+			UsageSessionService.login(user.getId(), eid, ipAddress, null, UsageSessionService.EVENT_LOGIN_WS);
+			return s.getId();
+		}
+		
 	}
 
 }
---
+++ axis2/src/main/java/org/sakaiproject/axis2/TimeCalculator.java
@@ -0,0 +1,74 @@
+package org.sakaiproject.axis2;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class TimeCalculator {
+	
+	public static SimpleDateFormat iso8601Format = new SimpleDateFormat("yyyy-MM-ddHH:mm:ssZ");
+	private static final int MS_PER_DAY = (24 * 60 * 60 *1000);
+
+	/**
+	 * 
+	 * @param iso8601dateString Should look like 2008-06-0313:51:20Z
+	 * @return 
+	 */
+	public static Date parseDate(String iso8601dateString) {
+		int tee = iso8601dateString.indexOf('T');
+		int zed = iso8601dateString.indexOf('Z');
+		
+		String pattern = iso8601dateString.substring(0, tee) + iso8601dateString.substring(tee+1, zed) + "GMT";
+		
+		Date date = null;
+		try {
+			date = iso8601Format.parse(pattern);
+		}
+		catch(ParseException e) {}
+		return date;
+		
+	}
+	
+	/**
+	 * 
+	 * @param iso8601dateString Should look like 2008-06-03T13:51:20Z
+	 * @return The difference (in milliseconds) between the time defined by 
+	 * 			iso8601dateString and the current time. Positive if the current
+	 * 			time comes after the time described by iso8601dateString
+	 */
+	public static long getTimeDifference(String iso8601dateString) {
+		Date other = parseDate(iso8601dateString);
+
+		return other.getTime() - System.currentTimeMillis();
+	}
+	
+	public static boolean within2Days(String iso8601dateString) {
+		return Math.abs(getTimeDifference(iso8601dateString)) < 2*MS_PER_DAY;
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		
+		long cur = (System.currentTimeMillis()/1000)*1000;	//adjust for lack of milliseconds
+		Date d = new Date(cur);
+		String teststring = iso8601Format.format(cur);
+		teststring = teststring.substring(0, 10) + "T" + teststring.substring(10,18) + "Z";
+		
+		System.out.println(teststring);
+		System.out.println(getTimeDifference(teststring)/60000 + "minutes from gmt");
+		System.out.println(cur - System.currentTimeMillis());
+		
+		String far = "2008-06-03T13:51:20Z";
+		String close = "2008-06-20T13:51:20Z";
+		
+		System.out.println("MS away from close date: " + getTimeDifference(close));
+		System.out.println("Minutes: " + (getTimeDifference(close) / 60000));
+		System.out.println("close date is close: " + within2Days(close));
+		
+		System.out.println("MS away from far date: " + getTimeDifference(far));
+		System.out.println("Minutes: " + (getTimeDifference(far) / 60000));
+		System.out.println("far date is close: " + within2Days(far));
+	}
+
+}
