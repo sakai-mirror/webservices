d9e55d62049fe125427be86f1d29113e267efad8
katherine.edwards2@mail.mcgill.ca <katherine.edwards2@mail.mcgill.ca@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2008-08-19 20:05:57 +0000

    added service java file...subclipse is giving me major headaches which is the reason for missing files!
    git-svn-id: https://source.sakaiproject.org/svn/webservices/branches/SAK-13392-B@50992 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ axis2/src/main/java/org/sakaiproject/axis2/MyService.java
@@ -0,0 +1,26 @@
+package org.sakaiproject.axis2;
+
+import javax.xml.namespace.QName;
+import javax.xml.stream.XMLStreamException;
+import org.apache.axiom.om.OMAbstractFactory;
+import org.apache.axiom.om.OMElement;
+import org.apache.axiom.om.OMFactory;
+
+public class MyService {
+	
+	public OMElement echo(OMElement el) throws XMLStreamException {
+		el.build();
+		el.detach();
+			
+		OMElement child = el.getFirstChildWithName(new QName("name"));
+		String person = child.getText();
+		
+		OMFactory factory = OMAbstractFactory.getOMFactory();
+		
+		OMElement reply = factory.createOMElement(new QName("reply"));
+		reply.setText("Hello, " + person);
+		
+		return reply;
+	}
+
+}
